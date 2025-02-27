47bbd56418af5be2c1bcfd263ee9b3e70b3d0efa
adryborja <amborja1@espe.edu.ec>
2025-02-27 03:59:57 -0500

    arreglo usuarios

--- src/pages/Usuarios.tsx
+++ src/pages/Usuarios.tsx
@@ -3,12 +3,9 @@ import { UserList } from "../components/Usuarios/UserList";
 
 
 export const Usuarios: React.FC = () => {
-
   return (
     <div>
       <h1>Gestión de Usuarios</h1>
-      
-      
       <UserList />
 
     </div>
